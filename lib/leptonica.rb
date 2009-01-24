ea4322653f417b000e96d9666c4cf43a0317fd39
Jay Anderson <horndude77@gmail.com>
2009-01-23 20:43:03 -0700

    Forgot to delete the pointer class from the main leptonica file.

--- lib/leptonica.rb
+++ lib/leptonica.rb
@@ -6,17 +6,3 @@ require 'box'
 require 'sel'
 require 'num'
 
-module Leptonica
-    class PointerClass
-        attr_reader :pointer
-        def initialize(pointer)
-            @pointer = pointer
-            ObjectSpace.define_finalizer(self, PointerClass.release(pointer, self.class))
-        end
-
-        def self.release(pointer, clazz)
-            proc {|id| clazz.release(pointer)}
-        end
-    end
-
-end
